4178eaed12399b240670ee1b8d4834c2883cedb4
Federico Mena Quintero <federico@gnome.org>
2026-09-08 12:03:02 -0600

    ci/setup-dependencies-env.sh: Set the Rust stack size to 8 MB for the test suite
    The next commit will raise the maximum number of nested layers to 500
    for glibc systems, and this requires more stack space than Rust
    normally allocates for the threads in 'cargo test'.
    
    Strictly speaking, this amount of stack is only required in debug
    mode.  In release mode, the amount required is much lower, and the
    test suite works fine with the default stack size.  We use a larger
    stack here so that the test suite can be run in debug mode as usual.
    
    The expectation is that production builds *are* in release mode, and
    thus require a small stack size, so changing it then should not be
    needed.
    
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1209>

--- ci/setup-dependencies-env.sh
+++ ci/setup-dependencies-env.sh
@@ -15,3 +15,9 @@ export LD_LIBRARY_PATH=$PREFIX/lib64
 export PKG_CONFIG_PATH=$PREFIX/lib64/pkgconfig
 export XDG_DATA_DIRS=${PREFIX}/share:/usr/share
 export ACLOCAL_PATH=${PREFIX}/share/aclocal
+
+# For bug #1224, librsvg raised the maximum number of nested layers in
+# SVG documents so that running the test suite in debug mode now
+# requires about 5 MB of stack on each thread that 'cargo test' uses.
+# Set it to 8 MB for safety.
+export RUST_MIN_STACK=8388608
